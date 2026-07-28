a0e93bb44764e16b527e3fb8c1bc9981627c6ad5
mhmdgg3 <68659385+mhmdgg3@users.noreply.github.com>
2026-07-28 21:41:17 +0300

    Update mail.js

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,22 +1,13 @@
 import nodemailer from "nodemailer";
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? "EXISTS" : "MISSING");
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // لازم تكون false مع 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});
-
-transporter.verify((error) => {
-  if (error) {
-    console.log("MAIL ERROR:", error);
-  } else {
-    console.log("MAIL READY");
-  }
 });
 
 export default transporter;
