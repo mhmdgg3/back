fc63b9adfd5f6781b0bee804bdf698f3cc444577
mhmdgg3 <68659385+mhmdgg3@users.noreply.github.com>
2026-07-28 21:29:36 +0300

    Update email.services.js

--- src/services/email.services.js
+++ src/services/email.services.js
@@ -1,45 +1,40 @@
 import transporter from "../config/mail.js";
 
 export async function sendVerificationEmail(email, code) {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Verify your email",
-    html: `
-        
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Verify your email",
+      html: `
         <h2>Email Verification</h2>
         <p>Your verification code is:</p>
         <h1>${code}</h1>
         <p>This code will expire in 10 minutes.</p>
-        `,
-  });
+      `,
+    });
+
+    console.log("EMAIL SENT:", info.messageId);
+
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
+    throw error;
+  }
 }
 
 export async function sendVerificationPassword(email, code) {
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "Password Reset Code",
-    html: `
-  <h2>Reset Your Password</h2>
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Password Reset Code",
+      html: `...`,
+    });
 
-  <p>Use the verification code below to reset your password:</p>
+    console.log("EMAIL SENT:", info.messageId);
 
-  <div style="
-    font-size:32px;
-    font-weight:bold;
-    text-align:center;
-    background:#f4f4f4;
-    padding:20px;
-    border-radius:8px;
-    letter-spacing:6px;
-  ">
-    ${code}
-  </div>
-
-  <p>This verification code is valid for <strong>10 minutes</strong>.</p>
-
-  <p>If you did not request a password reset, please ignore this email. Your account will remain secure.</p>
-`,
-  });
+  } catch (error) {
+    console.log("EMAIL ERROR:", error);
+    throw error;
+  }
 }
